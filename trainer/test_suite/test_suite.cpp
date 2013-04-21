c4414687c85e1e22551e5ddacea4c6961970977a
Cody De La Vara <codygeronimo@gmail.com>
2013-04-21 00:01:58 -0400

    implemented rough cross-verification. I think there is a bug in the way I predict. The code gets 10% of the data and tests it against hte other 90, and does this for each 10%

--- trainer/test_suite/test_suite.cpp
+++ trainer/test_suite/test_suite.cpp
@@ -50,61 +50,79 @@ int main (int argc, char *argv[])
 
     /* load in images */
     int nr_class = FileUtils::read_images(argv[1], images, labels, names);
-
     /* take one image out of the set randomly and test it against the training set */
 
     vector<string> images_used; /* keep track of images we used so we don't repeat */
-
-    /* main loop for testing */
-    for (int i = 0; i < number_of_tests; ++i) {
-    int random_index = rand() % images.size();
-
-    /* extract a random image, along with its name and label */
-    Mat image = images[random_index];
-    int label = labels[random_index];
-    string name = names[random_index];
-
-    cout << "Testing " << name << " against training set" << endl;
-
-    /* remove items from their respective vectors */
-    images.erase(images.begin() + random_index);
-    labels.erase(labels.begin() + random_index);
-    names.erase(names.begin() + random_index);
-
-    /* now build all the features with remainder of images */
-    vector<Mat> features;
-    featureExtractor->extract_features_batch(images, features);
-
-    Mat train_data;
-    vector<int> features_img_labels;
-    int features_size;
-
-    /* build a column-vector that has maps image labels to descriptors */
-    for (int j = 0; j < features.size(); j++)
-    {
-      train_data.push_back(features[j]);
-      features_size = features[j].rows;
-      for (int i = 0; i < features_size; i++)
-      {
-        features_img_labels.push_back(labels[j]);
-      }
-      cout << "Finished Image: " << j << " of " << features.size() << endl;
+    vector<int> training_numbers;
+    for (int i = 0; i < images.size(); i++) {
+        training_numbers.push_back(i);
     }
 
-    cout << "Training vocab tree..." << endl;
+    /* main loop for testing */
+    for (int i = 0; i < 10; ++i) {
+        vector<Mat> test_images;
+        vector<int> test_labels;
+        vector<string> test_names;
 
-    //Set up the Vocab Tree Trainer
-    CvVocabTree *vocab_tree = new CvVocabTree();
-    vocab_tree->train(&train_data, features_img_labels, images.size());
-    Mat* results;
+        int num_images = images.size();
+        int steps = num_images / 10;
+        int j = 0;
 
-    
-    cout << "\"Predicting\" (on images I should know)..." << endl;
-    cout << "image 0 is image..." << vocab_tree->predict(&features[0], results) << endl;
-    cout << "image 1 is image..." << vocab_tree->predict(&features[1], results) << endl;
-    cout << "image 2 is image..." << vocab_tree->predict(&features[2], results) << endl;
-    cout << "image 3 is image..." << vocab_tree->predict(&features[3], results) << endl;
-    cout << "image 4 is image..." << vocab_tree->predict(&features[4], results) << endl;
-    cout << "image 5 is image..." << vocab_tree->predict(&features[5], results) << endl;     
+        for (j = i; j < num_images; j += steps) {
+            test_images.push_back(images[j]);
+            test_labels.push_back(labels[j]);
+            test_names.push_back(names[j]);
+        }
+
+        /* remove items from their respective vectors */
+        j = j - steps;
+        for (; j >= 0; j -= steps) {
+
+            images.erase(images.begin() + j);
+            labels.erase(labels.begin() + j);
+            names.erase(names.begin() + j);
+        }   
+
+        /* now build all the features with remainder of images */
+        vector<Mat> training_features;
+        featureExtractor->extract_features_batch(images, training_features);
+
+        vector<Mat> test_features;
+        featureExtractor->extract_features_batch(test_images, test_features);
+
+        Mat train_data;
+        vector<int> features_img_labels;
+        int features_size;
+
+        /* build a column-vector that has maps image labels to descriptors */
+        for (int j = 0; j < training_features.size(); j++)
+        {
+          train_data.push_back(training_features[j]);
+          features_size = training_features[j].rows;
+          for (int i = 0; i < features_size; i++)
+          {
+            features_img_labels.push_back(labels[j]);
+          }
+          cout << "Finished Image: " << j << " of " << training_features.size() << endl;
+        }
+
+        cout << "Training vocab tree..." << endl;
+
+        //Set up the Vocab Tree Trainer
+        CvVocabTree *vocab_tree = new CvVocabTree();
+        vocab_tree->train(&train_data, features_img_labels, images.size());
+        Mat* results;
+
+        int num_test_images = test_images.size();
+        for (int i = 0; i < num_test_images; i++) {
+            cout << "\"Predicting\" (on images I should know)..." << endl;
+            cout << "image " << test_labels[i] << " is image... " << labels[vocab_tree->predict(&test_features[i], results)] << endl;
+        }
+
+        for (j = i; j < num_images; j += steps) {
+            images.insert(images.begin() + j, test_images[ j / steps]);
+            labels.insert(labels.begin() + j, test_labels[j / steps]);
+            names.insert(names.begin() + j, test_names[j / steps]);
+        }
   }
 }
