0e2952b44bbf558387499e219c6ed64d5735f9ce
Matthew Piziak <Matthew Piziak>
2013-04-23 20:39:50 -0400

    Does all slices instead of just one.

--- trainer/test_suite/test_suite.cpp
+++ trainer/test_suite/test_suite.cpp
@@ -33,8 +33,8 @@ int rand_gen (int i){
 int main (int argc, char *argv[])
 {
     if (argc != 2 && argc != 4) {
-      cerr << "usage: test_suite <image-root> [model-output vocab-output]" << endl;
-      return -1;
+	cerr << "usage: test_suite <image-root> [model-output vocab-output]" << endl;
+	return -1;
     }
     char *image_root = argv[1];
     char *model_output = NULL;
@@ -55,8 +55,6 @@ int main (int argc, char *argv[])
     float current_accuracy = 1.0; //update on each round.
     float round = 0.0;
       
-    while(1){ // this goes on forever for now; updating to a deterministic system soon
-
     // deterministically shuffling the vectors
     srand(6421);
     random_shuffle(images.begin(), images.end(), rand_gen);
@@ -65,61 +63,60 @@ int main (int argc, char *argv[])
     srand(6421);
     random_shuffle(names.begin(), names.end(), rand_gen);
     
-    // extract test images 
-    int test_size = images.size() / TEST_FRACTION;
-    vector<Mat> test_images(images.begin() + 0, images.begin() + test_size);
-    images.erase(images.begin() + 0, images.begin() + test_size);
-    vector<int> test_labels(labels.begin() + 0, labels.begin() + test_size);
-    labels.erase(labels.begin() + 0, labels.begin() + test_size);
-    vector<string> test_names(names.begin() + 0, names.begin() + test_size);
-    names.erase(names.begin() + 0, names.begin() + test_size);
+    int test_round = 0;
+    for(; test_round < TEST_FRACTION; test_round++){
+        // extract test images 
+	int test_size = images.size() / TEST_FRACTION;
+	int test_offset = test_size * test_round;
+	vector<Mat> test_images(images.begin() + test_offset, images.begin() + test_offset + test_size);
+	images.erase(images.begin() + test_offset, images.begin() + test_offset + test_size);
+	vector<int> test_labels(labels.begin() + test_offset, labels.begin() + test_offset + test_size);
+	labels.erase(labels.begin() + test_offset, labels.begin() + test_offset + test_size);
+	vector<string> test_names(names.begin() + test_offset, names.begin() + test_offset + test_size);
+	names.erase(names.begin() + test_offset, names.begin() + test_offset + test_size);
 
-    // extract test features
-    vector<Mat> test_features;
-    featureExtractor->extract_features_batch(test_images, test_features);
+	// extract test features
+	vector<Mat> test_features;
+	featureExtractor->extract_features_batch(test_images, test_features);
 
-    // extract training features
-    cout << "Extracting features..." << endl;
-    vector<Mat> features;
-    featureExtractor->extract_features_batch(images, features);
+	// extract training features
+	cout << "Extracting features..." << endl;
+	vector<Mat> features;
+	featureExtractor->extract_features_batch(images, features);
 
-    // Concatenate the vector of Mats into a big Mat file
-    Mat train_data;
-    vector<int> features_img_labels;
-    int features_size;
-    for (int j = 0; j < features.size(); j++) {
-      train_data.push_back(features[j]);
-      features_size = features[j].rows;
-      for (int i = 0; i < features_size; i++) {
-	features_img_labels.push_back(j);
-      }
-    } 
+	// Concatenate the vector of Mats into a big Mat file
+	Mat train_data;
+	vector<int> features_img_labels;
+	int features_size;
+	for (int j = 0; j < features.size(); j++) {
+	    train_data.push_back(features[j]);
+	    features_size = features[j].rows;
+	    for (int i = 0; i < features_size; i++) {
+		features_img_labels.push_back(j);
+	    }
+	} 
 
-    cout << "Extracted " << train_data.rows << " features." << endl;
+	cout << "Extracted " << train_data.rows << " features." << endl;
 
-    cout << "Training vocab tree..." << endl;
-    //Set up the Vocab Tree Trainer
-    CvVocabTree *vocab_tree = new CvVocabTree();
-    vocab_tree->train(&train_data, features_img_labels, images.size());
-    Mat* results;
+	cout << "Training vocab tree..." << endl;
+	//Set up the Vocab Tree Trainer
+	CvVocabTree *vocab_tree = new CvVocabTree();
+	vocab_tree->train(&train_data, features_img_labels, images.size());
+	Mat* results;
 
-    cout << "Predicting ..." << endl;
+	cout << "Predicting ..." << endl;
     
 	float num_correct = 0;
-    for(vector<int>::size_type i = 0; i != test_images.size(); i++) {
-        int result = vocab_tree->predict(&test_features[i], results);
-		cout << "The predicted building was: " << labels[result] << "The actual building was: " << test_labels[i]; 
-		if (labels[result] == test_labels[i]){
-			num_correct++;
-		} 
+	for(vector<int>::size_type i = 0; i != test_images.size(); i++) {
+	    int result = vocab_tree->predict(&test_features[i], results);
+	    cout << "The predicted building was: " << labels[result] 
+		 << "The actual building was: " << test_labels[i]; 
+	    if (labels[result] == test_labels[i]){
+		num_correct++;
+	    } 
+	}
+	cout << "The accuracy of this calculation is: " 
+	     << num_correct/(float)test_labels.size() << endl;
     }
-	
-	
-	cout << "The accuracy of this calculation is: " << num_correct/(float)test_labels.size();
-	    // labels.insert(labels.begin() + k, test_label);
-	    // names.insert(names.begin() + k, test_name);
-	    // images.insert(images.begin() + k, test_image);
-}
-    
     return 0;
 }
