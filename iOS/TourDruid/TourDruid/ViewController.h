5626dbadc076eb540cd36de6d9c6058ab433f620
Cody De La Vara <codygeronimo@gmail.com>
2013-04-23 20:58:08 -0400

    basic iOS app skeleton with images and junk

---
+++ iOS/TourDruid/TourDruid/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TourDruid
+//
+//  Created by Cody Geronimo De La Vara on 4/23/13.
+//  Copyright (c) 2013 Kin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOS/TourDruid/TourDruid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TourDruid
+//
+//  Created by Cody Geronimo De La Vara on 4/23/13.
+//  Copyright (c) 2013 Kin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
