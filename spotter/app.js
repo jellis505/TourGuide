bdde1178013eef846f916f77cf8c9c7f6cbede36
Peter Sugihara <peter@campsh.com>
2013-05-01 14:18:50 -0400

    added nodejs server

---
+++ spotter/app.js
@@ -0,0 +1,37 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , classifier = require('./routes/classifier')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: '/tmp' }));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+// app.post('/image/classify', classifier.classify);
+// app.post('/image/confirm', classifier.classify);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ spotter/routes/classifier.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.classify = function (req, res) {
+  res.send("respond with a resource");
+};
+
+exports.confirm = function (req, res) {
+  res.send("respond with a resource");
+};
