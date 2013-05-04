519584ea180c045978cfa6984f8e8d481e9e9631
Joe Ellis <joe.ellis.505@gmail.com>
2013-05-04 13:40:51 -0400

    Added the reading and writing functionality

--- trainer/test_suite/test_suite.cpp
+++ trainer/test_suite/test_suite.cpp
@@ -131,7 +131,7 @@ int main (int argc, char *argv[])
 	float num_correct = 0.0;
 	float per_building_correct[nr_class];
 	float per_building_count[nr_class];
-	string per_building_names[nr_class];
+	vector <string> per_building_names;
 	fill_n(per_building_correct, nr_class, 0.0);
 	fill_n(per_building_count, nr_class, 0.0);
 	for(int i = 0; i < test_images.size() && i < test_features.size(); i++) {
